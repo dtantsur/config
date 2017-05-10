98726487a5094b403abf14b7333178263087a06f
Dmitry Tantsur <divius.inside@gmail.com>
2017-05-10 15:24:47 +0200

    Support installing openstack repos

--- ansconf.py
+++ ansconf.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("inventory")
 parser.add_argument("--ssh-config")
 parser.add_argument("-v", "--verbose", action='store_true')
+parser.add_argument("-r", "--openstack-repo")
 args = parser.parse_args()
 
 
@@ -18,10 +19,21 @@ if not os.path.exists(inventory):
     inventory = inventory + ','
 
 call = ['ansible-playbook', 'configure.yml', '-i', inventory]
+extra_vars = []
+tags = ["untagged"]
 if args.ssh_config:
     call.extend(["--ssh-common-args", "-F %s" % args.ssh_config])
 if args.verbose:
     call.append('-vvvv')
+if args.openstack_repo:
+    tags.append('repos')
+    if args.openstack_repo != 'master':
+        extra_vars.append('openstack_release=%s' % args.openstack_repo)
+    else:
+        extra_vars.append('openstack_release=')
+
+call.extend(['--tags', ','.join(tags),
+             '--extra-vars', ' '.join(extra_vars)])
 
 try:
     subprocess.check_call(call)
