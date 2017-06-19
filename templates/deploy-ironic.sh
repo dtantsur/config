1251ef1a355e813a8001f48581a1e36976c6eb4f
Dmitry Tantsur <divius.inside@gmail.com>
2017-06-19 17:35:21 +0200

    deploy-ironic: disable Telemetry

--- templates/deploy-ironic.sh
+++ templates/deploy-ironic.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+set -eu
+
 TEMPLATES=${1:-/usr/share/openstack-tripleo-heat-templates}
 CONFIG=~/ironic-config.yaml
+
+if [ -f "$TEMPLATES/environments/disable-telemetry.yaml" ]; then
+    DISABLE_TELEMETRY="-e $TEMPLATES/environments/disable-telemetry.yaml"
+else
+    DISABLE_TELEMETRY=
+fi
 
 source ~/stackrc
 if ! openstack overcloud deploy --templates $TEMPLATES \
     -e $TEMPLATES/environments/puppet-pacemaker.yaml \
     -e $TEMPLATES/environments/services/ironic.yaml \
-    -e $CONFIG --ntp-server pool.ntp.org;
+    $DISABLE_TELEMETRY -e $CONFIG --ntp-server pool.ntp.org;
 then
     echo DEPLOY FAILED
     openstack stack failures list overcloud
