6975e6dc4cea4634c05967aed6de09cbf7515daf
Dmitry Tantsur <dtantsur@protonmail.com>
2020-11-27 15:13:24 +0100

    config.fish: changed prompt

--- config.fish
+++ config.fish
@@ -8,32 +8,23 @@ if test -f /usr/bin/vimx
     end
 end
 
-function fish_right_prompt
+function fish_prompt
+    set last_st $status
+
+    echo -n (set_color green)
+    printf '%s' (prompt_pwd)
+
     if git status > /dev/null 2> /dev/null
         if test (git status --short -uno | wc -l) = 0
-            echo -n (set_color green)
+            echo -n (set_color blue)
         else
             echo -n (set_color yellow)
         end
 
         if git show HEAD > /dev/null 2>&1
-            git rev-parse --abbrev-ref HEAD
-        else
-            echo "---"
+            printf ' (%s) ' (git rev-parse --abbrev-ref HEAD)
         end
     end
-end
-
-function fish_prompt
-    set last_st $status
-
-    set root_dir ~
-    set curr_dir (pwd)
-    set prog "import os.path; print(os.path.relpath('$curr_dir', '$root_dir') if '$root_dir' in '$curr_dir' else '$curr_dir')"
-    echo -n (set_color green)
-    echo -n '['
-    echo -n (python -c "$prog")
-    echo -n ']'
 
     if test $last_st = 0
         echo -n (set_color green)
